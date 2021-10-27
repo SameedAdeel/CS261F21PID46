f5f048a10dcfe8ffc0589f0f120efd3ae1ce1ebd
Muhammad-Usman-Rasheed <ur9191633@gmail.com>
2021-10-27 11:13:13 +0500

    Sorting Algorithems

--- lib.py
+++ lib.py
@@ -73,6 +73,85 @@ def Search(Arr,s):
             continue
     return list
 
+from math import floor
+def Bucket(arr,n):
+    bucket=[[] for f in range (n)]
+    output=[]
+    for i in range (0,n-1):
+        bucket[(floor(n*arr[i]))].append(arr[i])
+    
+    for i in range (0,len(bucket)):
+        InsertionSort(bucket[i])
+    for each in bucket:
+        output += each
+    return output
+
+def ConuntingSort(arr):
+    k= max(arr)-min(arr)
+    size=len(arr)
+    count=[0]*(k+1)
+    output=[0]*size
+    index=-1
+    P=[]
+    N=[]
+    for i in range (size):
+        j=arr[i]
+        count[j]+=1
+    
+    for i in range (1,k+1):
+        count[i]+=count[i-1]
+    
+    for i in range (size-1,-1,-1):
+        j = arr[i]
+        count[j]-=1
+        output[count[j]]=arr[i]
+    for i in range (0,size):
+        if output[i] >= 0 :
+            P.append(output[i])
+        else:
+            N.append(output[i])
+    print(P)
+    print(N)
+    return N+P
+
+def partition(arr,low,high):
+    
+    pivot=arr[high]
+    
+    i=low-1
+    
+    for j in range (low,high):
+        
+        if arr[j] < pivot :
+            i = i+1
+            arr[i],arr[j]=arr[j],arr[i]
+    
+    
+    arr[i+1],arr[high]=arr[high],arr[i+1]
+    
+    return i+1
+
+def QuickSelect(arr,left,right,k):
+    if left == right:
+        return arr[left]
+    print(right)
+    pivotIndex = partition(arr,left,right)
+    if k == pivotIndex :
+        return arr[k-1]
+    elif k < pivotIndex :
+        right = pivotIndex -1
+        return(quickSelect(arr,left,pivotIndex-1,k))
+    
+    else :
+        left = pivotIndex+1
+        return(quickSelect(arr,pivotIndex+1,right,k))
+
+
+def QuickSort(arr,low,high):
+    if low < high :
+        pi= partition(arr,low,high)
+        QuickSort(arr,low,pi-1)
+        QuickSort(arr,pi+1,high)
 
 
    
